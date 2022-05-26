cf72dfedfca1f8f18fbc4f57c5fc4ecd3126c308
tjoeatja <106184704+tjoeatja@users.noreply.github.com>
2022-05-26 15:29:27 -0400

    Update.variable.java

--- src/Variables.java
+++ src/Variables.java
@@ -21,19 +21,19 @@ public class Variables
         System.out.println("It has a minimum value of -32,768 and a maximum value of 32,767");
         
         System.out.println("By default, the int data type is a 32-bit signed two's complement integer");
-        System.out.println("int data type to represent an unsigned 32-bit integer, which has a minimum value of 0 and a maximum value of 232-1");
+        System.out.println("int data type to represent an unsigned 32-bit integer, which has a minimum");
         
         System.out.println("The long data type is a 64-bit two's complement integer");
         System.out.println("The signed long has a minimum value of -263 and a maximum value of 263-1");
         
         System.out.println("The float data is a single-precision 32-bit IEEE 754 floating point");
-        System.out.println(" Its range of values is beyond the scope of this discussion, but is specified in the Floating-Point Types, Formats,and Values");
+        System.out.println(" Its range of values is beyond the scope of this discussion, but is specified");
         
         System.out.println("The double data type is a double-precision 64-bit IEEE 754 floating point");
         System.out.println("For decimal values, this data type is generally the default choice");
         
         System.out.println("The boolean data type has only two possible values: true and false");
-        System.out.println("This data type represent one bit of information, but its size isn't something that's precisely defined");
+        System.out.println("This data type represent one bit of information, but its size isn't something");
         
         System.out.println("The char data type is a single 16-bit Unicode character");
         System.out.println("It has a minimum value of '\u0000' and a maximum value of '\uffff'");
